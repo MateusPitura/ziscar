1226e8b24808c7192b4544b5e1ac59ff4e4c2aa3
MateusPitura <mateuspitura@gmail.com>
2025-01-14 13:56:45 -0300

    feat: add design system button

--- frontend/src/design-system/Button.tsx
+++ frontend/src/design-system/Button.tsx
@@ -1,5 +1,124 @@
-import { Button } from "@/components/ui/button";
+import classNames from "classnames";
 
-export default function DsButton() {
-  return <Button variant="outline">Button</Button>;
+interface BaseButtonProps {
+  state?: "active" | "disabled" | "red" | "loading";
+  className?: string;
+  label?: string;
+  onClick: () => void;
+  fullWidth?: boolean;
+  textAlign?: "start" | "center" | "end";
+}
+
+function BaseButton({
+  state = undefined,
+  className,
+  label,
+  onClick,
+  fullWidth,
+  textAlign = "start",
+}: BaseButtonProps) {
+  return (
+    <button
+      className={classNames(
+        "px-4 rounded-md h-10 hover:opacity-50",
+        className,
+        {
+          "pointer-events-none opacity-50": state === "disabled",
+          "pointer-events-none animate-pulse": state === "loading",
+          "w-full": fullWidth,
+        }
+      )}
+      onClick={onClick}
+    >
+      <span
+        className={classNames("text-label-large flex", {
+          "justify-center": textAlign === "center",
+          "justify-start": textAlign === "start",
+          "justify-end": textAlign === "end",
+        })}
+      >
+        {label}
+      </span>
+    </button>
+  );
+}
+
+interface ButtonProps extends Omit<BaseButtonProps, "className"> {
+  variant: "primary" | "secondary" | "tertiary" | "quaternary";
+}
+
+export default function Button({
+  variant = "primary",
+  state = undefined,
+  label,
+  onClick,
+  fullWidth,
+  textAlign,
+}: ButtonProps) {
+  switch (variant) {
+    case "primary":
+      return (
+        <BaseButton
+          className={classNames("bg-light-primary text-light-onPrimary", {
+            "bg-light-secondary": state === "active",
+            "bg-light-error": state === "red",
+            "bg-light-outline": state === "disabled" || state === "loading",
+          })}
+          onClick={onClick}
+          state={state}
+          label={label}
+          fullWidth={fullWidth}
+          textAlign={textAlign}
+        />
+      );
+    case "secondary":
+      return (
+        <BaseButton
+          className={classNames(
+            "border-light-primary border-2 text-light-primary",
+            {
+              "bg-light-primaryContainer": state === "active",
+              "border-light-error text-light-error": state === "red",
+              "border-light-outline text-light-outline":
+                state === "disabled" || state === "loading",
+            }
+          )}
+          onClick={onClick}
+          state={state}
+          label={label}
+          fullWidth={fullWidth}
+          textAlign={textAlign}
+        />
+      );
+    case "tertiary":
+      return (
+        <BaseButton
+          className={classNames("text-light-onSurface", {
+            "text-light-primary": state === "active",
+            "text-light-error": state === "red",
+            "text-light-outline": state === "disabled" || state === "loading",
+          })}
+          onClick={onClick}
+          state={state}
+          label={label}
+          fullWidth={fullWidth}
+          textAlign={textAlign}
+        />
+      );
+    case "quaternary":
+      return (
+        <BaseButton
+          className={classNames("text-light-primary", {
+            "bg-light-primaryContainer": state === "active",
+            "text-light-error": state === "red",
+            "text-light-outline": state === "disabled" || state === "loading",
+          })}
+          onClick={onClick}
+          state={state}
+          label={label}
+          fullWidth={fullWidth}
+          textAlign={textAlign}
+        />
+      );
+  }
 }
--- frontend/src/pages/users/UsersPage.tsx
+++ frontend/src/pages/users/UsersPage.tsx
@@ -1,3 +1,277 @@
+import Button from "@/design-system/Button";
+
 export default function UsersPage() {
-  return <div className="bg-red-500">Users Page</div>;
+  return (
+    <>
+      Variants
+      <div className="flex gap-2 p-2">
+        <Button
+          variant="primary"
+          label="Button"
+          onClick={() => {
+            alert("teste");
+          }}
+        />
+        <Button variant="secondary" label="Button" onClick={() => {}} />
+        <Button variant="tertiary" label="Button" onClick={() => {}} />
+        <Button variant="quaternary" label="Button" onClick={() => {}} />
+        <div className="bg-blue-100 p-2 gap-2 flex">
+          <Button variant="primary" label="Button" onClick={() => {}} />
+          <Button variant="secondary" label="Button" onClick={() => {}} />
+          <Button variant="tertiary" label="Button" onClick={() => {}} />
+          <Button variant="quaternary" label="Button" onClick={() => {}} />
+        </div>
+      </div>
+      Active
+      <div className="flex gap-2 p-2">
+        <Button
+          variant="primary"
+          state="active"
+          label="Button"
+          onClick={() => {}}
+        />
+        <Button
+          variant="secondary"
+          state="active"
+          label="Button"
+          onClick={() => {}}
+        />
+        <Button
+          variant="tertiary"
+          state="active"
+          label="Button"
+          onClick={() => {}}
+        />
+        <Button
+          variant="quaternary"
+          state="active"
+          label="Button"
+          onClick={() => {}}
+        />
+        <div className="bg-blue-100 p-2 gap-2 flex">
+          <Button
+            variant="primary"
+            state="active"
+            label="Button"
+            onClick={() => {}}
+          />
+          <Button
+            variant="secondary"
+            state="active"
+            label="Button"
+            onClick={() => {}}
+          />
+          <Button
+            variant="tertiary"
+            state="active"
+            label="Button"
+            onClick={() => {}}
+          />
+          <Button
+            variant="quaternary"
+            state="active"
+            label="Button"
+            onClick={() => {}}
+          />
+        </div>
+      </div>
+      Disabled
+      <div className="flex gap-2 p-2">
+        <Button
+          variant="primary"
+          state="disabled"
+          label="Button"
+          onClick={() => {}}
+        />
+        <Button
+          variant="secondary"
+          state="disabled"
+          label="Button"
+          onClick={() => {}}
+        />
+        <Button
+          variant="tertiary"
+          state="disabled"
+          label="Button"
+          onClick={() => {}}
+        />
+        <Button
+          variant="quaternary"
+          state="disabled"
+          label="Button"
+          onClick={() => {}}
+        />
+        <div className="bg-blue-100 p-2 gap-2 flex">
+          <Button
+            variant="primary"
+            state="disabled"
+            label="Button"
+            onClick={() => {}}
+          />
+          <Button
+            variant="secondary"
+            state="disabled"
+            label="Button"
+            onClick={() => {}}
+          />
+          <Button
+            variant="tertiary"
+            state="disabled"
+            label="Button"
+            onClick={() => {}}
+          />
+          <Button
+            variant="quaternary"
+            state="disabled"
+            label="Button"
+            onClick={() => {}}
+          />
+        </div>
+      </div>
+      Error
+      <div className="flex gap-2 p-2">
+        <Button
+          variant="primary"
+          state="red"
+          label="Button"
+          onClick={() => {}}
+        />
+        <Button
+          variant="secondary"
+          state="red"
+          label="Button"
+          onClick={() => {}}
+        />
+        <Button
+          variant="tertiary"
+          state="red"
+          label="Button"
+          onClick={() => {}}
+        />
+        <Button
+          variant="quaternary"
+          state="red"
+          label="Button"
+          onClick={() => {}}
+        />
+        <div className="bg-blue-100 p-2 gap-2 flex">
+          <Button
+            variant="primary"
+            state="red"
+            label="Button"
+            onClick={() => {}}
+          />
+          <Button
+            variant="secondary"
+            state="red"
+            label="Button"
+            onClick={() => {}}
+          />
+          <Button
+            variant="tertiary"
+            state="red"
+            label="Button"
+            onClick={() => {}}
+          />
+          <Button
+            variant="quaternary"
+            state="red"
+            label="Button"
+            onClick={() => {}}
+          />
+        </div>
+      </div>
+      Loading
+      <div className="flex gap-2 p-2">
+        <Button
+          variant="primary"
+          state="loading"
+          label="Button"
+          onClick={() => {}}
+        />
+        <Button
+          variant="secondary"
+          state="loading"
+          label="Button"
+          onClick={() => {}}
+        />
+        <Button
+          variant="tertiary"
+          state="loading"
+          label="Button"
+          onClick={() => {}}
+        />
+        <Button
+          variant="quaternary"
+          state="loading"
+          label="Button"
+          onClick={() => {}}
+        />
+        <div className="bg-blue-100 p-2 gap-2 flex">
+          <Button
+            variant="primary"
+            state="loading"
+            label="Button"
+            onClick={() => {}}
+          />
+          <Button
+            variant="secondary"
+            state="loading"
+            label="Button"
+            onClick={() => {}}
+          />
+          <Button
+            variant="tertiary"
+            state="loading"
+            label="Button"
+            onClick={() => {}}
+          />
+          <Button
+            variant="quaternary"
+            state="loading"
+            label="Button"
+            onClick={() => {}}
+          />
+        </div>
+      </div>
+      Full
+      <div className="flex gap-2 p-2">
+        <div className="bg-green-200 w-40">
+          <Button
+            fullWidth
+            variant="primary"
+            label="Button"
+            onClick={() => {}}
+            textAlign="start"
+          />
+        </div>
+        <div className="bg-green-200 w-40">
+          <Button
+            fullWidth
+            variant="secondary"
+            label="Button"
+            onClick={() => {}}
+            textAlign="center"
+          />
+        </div>
+        <div className="bg-green-200 w-40">
+          <Button
+            fullWidth
+            variant="tertiary"
+            label="Button"
+            onClick={() => {}}
+            textAlign="end"
+            />
+        </div>
+        <div className="bg-green-200 w-40">
+          <Button
+            fullWidth
+            variant="quaternary"
+            label="Button"
+            onClick={() => {}}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
