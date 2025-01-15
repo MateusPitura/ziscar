9df35b3052feab8a2c375caf8631544754969a8b
MateusPitura <mateuspitura@gmail.com>
2025-01-15 09:57:59 -0300

    fix: fix scroll in header and side menu

--- frontend/src/components/PageLayout.tsx
+++ frontend/src/components/PageLayout.tsx
@@ -11,10 +11,10 @@ export default function PageLayout({
 }: PageLayoutProperties): ReactElement {
   return (
     <div className="bg-light-surfaceContainerLowest w-full h-screen flex flex-col">
-      <PageHeader />
-      <div className="flex flex-1">
-        <PageSideMenu />
-        <div className="bg-light-surface h-full p-4 rounded-tl-md overflow-y-auto flex-1">
+      <PageHeader/>
+      <div className="flex flex-1 overflow-hidden">
+        <PageSideMenu/>
+        <div className="bg-light-surface h-full p-4 rounded-tl-md w-full overflow-y-auto">
           {children}
         </div>
       </div>
