4aa3424da15cb4022a2a3a33aab52c5b402999a6
MateusPitura <mateuspitura@gmail.com>
2025-01-15 11:22:51 -0300

    chore: removed test code

--- frontend/src/pages/users/UsersPage.tsx
+++ frontend/src/pages/users/UsersPage.tsx
@@ -1,11 +1,3 @@
-import PageHeader from "@/components/PageHeader";
-import PageSideMenu from "@/components/PageSideMenu";
-
 export default function UsersPage() {
-  return (
-    <div className="w-full bg-light-surface h-screen">
-      <PageHeader />
-      <PageSideMenu />
-    </div>
-  );
+  return <div>Users Page</div>;
 }
--- frontend/src/pages/vehicles/VehiclesPage.tsx
+++ frontend/src/pages/vehicles/VehiclesPage.tsx
@@ -1,8 +1,3 @@
-
 export default function VehiclesPage() {
-  return (
-    <>
-      <div className="bg-light-primary text-display-large">Vehicle Page</div>
-    </>
-  );
+  return <div>Vehicle Page</div>;
 }
