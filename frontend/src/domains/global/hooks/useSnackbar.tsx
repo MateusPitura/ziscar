9c1ecbad9a0ea65188f1e21e3fb55be7d6b6d5b2
MateusPitura <mateuspitura@gmail.com>
2025-01-16 09:55:42 -0300

    refactor: added useCallback to hook

--- frontend/src/domains/global/hooks/useSnackbar.tsx
+++ frontend/src/domains/global/hooks/useSnackbar.tsx
@@ -1,6 +1,7 @@
 import Button from "@/design-system/Button";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@radix-ui/react-toast";
+import { useCallback } from "react";
 
 interface ShowSnackbarProps {
   title: string;
@@ -12,51 +13,47 @@ interface ShowSnackbarProps {
 export default function useSnackbar() {
   const { toast } = useToast();
 
-  function showSuccessSnackbar({
-    title,
-    description,
-    onActionClick,
-    actionLabel,
-  }: ShowSnackbarProps) {
-    toast({
-      title,
-      description,
-      action: onActionClick && (
-        <ToastAction altText="Action button" asChild>
-          <Button
-            variant="quaternary"
-            label={actionLabel}
-            padding="none"
-            onClick={onActionClick}
-          />
-        </ToastAction>
-      ),
-    });
-  }
+  const showSuccessSnackbar = useCallback(
+    ({ title, description, onActionClick, actionLabel }: ShowSnackbarProps) => {
+      toast({
+        title,
+        description,
+        action: onActionClick && (
+          <ToastAction altText="Action button" asChild>
+            <Button
+              variant="quaternary"
+              label={actionLabel}
+              padding="none"
+              onClick={onActionClick}
+            />
+          </ToastAction>
+        ),
+      });
+    },
+    [toast]
+  );
 
-  function showErrorSnackbar({
-    title,
-    description,
-    onActionClick,
-    actionLabel,
-  }: ShowSnackbarProps) {
-    toast({
-      variant: "destructive",
-      title,
-      description,
-      action: onActionClick && (
-        <ToastAction altText="Action button" asChild>
-          <Button
-            variant="primary"
-            state="red"
-            label={actionLabel}
-            padding="none"
-            onClick={onActionClick}
-          />
-        </ToastAction>
-      ),
-    });
-  }
+  const showErrorSnackbar = useCallback(
+    ({ title, description, onActionClick, actionLabel }: ShowSnackbarProps) => {
+      toast({
+        variant: "destructive",
+        title,
+        description,
+        action: onActionClick && (
+          <ToastAction altText="Action button" asChild>
+            <Button
+              variant="primary"
+              state="red"
+              label={actionLabel}
+              padding="none"
+              onClick={onActionClick}
+            />
+          </ToastAction>
+        ),
+      });
+    },
+    [toast]
+  );
 
   return {
     showSuccessSnackbar,
