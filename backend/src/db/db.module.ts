29277c5ab5975c90f7cceab3cd2283129e0264ba
MateusPitura <mateuspitura@gmail.com>
2025-02-15 12:55:28 -0300

    feat: added typeorm and docker-compose

---
+++ backend/src/db/db.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          entities: [],
+          migrations: ['dist/db/migrations/*.js'],
+          synchronize: false,
+        };
+      },
+      inject: [ConfigService],
+      imports: [ConfigModule],
+    }),
+  ],
+})
+export class DbModule {}
