c775bc051aae68eca61acad92b897c049a8303f6
MateusPitura <mateuspitura@gmail.com>
2025-02-21 13:02:32 -0300

    chore: removed old projects

--- backend_v2/src/questions/dto/create-question.dto.ts
+++
@@ -1,6 +0,0 @@
-import { Prisma } from '@prisma/client';
-
-export type CreateQuestionDto = Omit<
-  Prisma.QuestionsCreateInput,
-  'createAt' | 'updateAt' | 'answers' | 'user'
->;
--- backend_v2/src/questions/dto/update-question.dto.ts
+++
@@ -1,6 +0,0 @@
-import { Prisma } from '@prisma/client';
-
-export type UpdateQuestionDto = Omit<
-  Prisma.QuestionsUpdateInput,
-  'createAt' | 'updateAt' | 'answers' | 'user'
->;
