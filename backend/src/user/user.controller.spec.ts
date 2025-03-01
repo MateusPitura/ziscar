7e4aefb565fea48d44ae6d496cf9606ab28c10c6
MateusPitura <mateuspitura@gmail.com>
2025-03-01 18:24:17 -0300

    test: added fetch users test

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -3,8 +3,8 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaService } from '../database/prisma.service';
 import { JwtService } from '@nestjs/jwt';
-import { POPULATE_USER_DEFAULT } from '../constants';
-import { GET_USER } from './user.constants';
+import { ITEMS_PER_PAGE, POPULATE_USER_DEFAULT } from '../constants';
+import { FETCH_USER, GET_USER } from './user.constants';
 
 describe('UserController', () => {
   let userController: UserController;
@@ -46,6 +46,31 @@ describe('UserController', () => {
       }
 
       transaction.rollback();
+    });
+  });
+
+  it('should find many users with many filters', async () => {
+    const spy = jest.spyOn(prismaService.user, 'findMany');
+
+    await userController.fetch({
+      page: 1,
+      status: 'active',
+      fullName: POPULATE_USER_DEFAULT.fullName,
+      orderBy: 'fullName',
+    });
+
+    expect(spy).toHaveBeenCalledWith({
+      skip: 0,
+      take: ITEMS_PER_PAGE,
+      where: {
+        fullName: {
+          contains: POPULATE_USER_DEFAULT.fullName.toLocaleLowerCase(),
+          mode: 'insensitive',
+        },
+        isActive: true,
+      },
+      orderBy: [{ fullName: 'asc' }],
+      select: FETCH_USER,
     });
   });
 });
