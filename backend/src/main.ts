cfb0641131760b139c2f9b7bf21a3528a2905e18
MateusPitura <mateuspitura@gmail.com>
2025-03-15 10:57:16 -0300

    feat: updated cors policy with frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://mateuspitura.github.io'],
+    origin: ['http://localhost:5173', 'https://ziscar.me'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
