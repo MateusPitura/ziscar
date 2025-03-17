39eb7ced957547b233321c5055d592aba8790ec0
MateusPitura <mateuspitura@gmail.com>
2025-03-16 22:05:49 -0300

    fix: added dynamic CSP and CORS policy based in dev or prod mode

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://ziscar.me'],
+    origin: [
+      process.env.NODE_ENV === 'production'
+        ? 'https://ziscar.me'
+        : 'http://localhost:5173',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
