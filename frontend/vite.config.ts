13a044257a18147f903c533a80e7658316cf9961
MateusPitura <mateuspitura@gmail.com>
2025-03-20 20:59:20 -0300

    refactor: avoid using .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { createHtmlPlugin } from "vite-plugin-html";
 
-export default () => {
+export default ({ mode }: { mode: string }) => {
   return defineConfig({
-    plugins: [react()],
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            BACKEND_URL:
+              mode === "production"
+                ? "https://api.ziscar.me"
+                : "http://localhost:3000",
+          },
+        },
+      }),
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
