2b647cbfbc297f986482e68ec1c6e9457f1798c5
Pedro Gabriel <pedrogabrielcordeiro09@gmail.com>
2025-08-12 23:09:09 -0300

    feat(backend): fix at the vehicle dtos creation

--- backend/src/entities/vehicle/dtos.ts
+++ backend/src/entities/vehicle/dtos.ts
@@ -13,50 +13,46 @@ import {
   UpdateVehicleRequestSchema,
   UpdateVehicleResponseSchema,
 } from '@shared/dtos/vehicle.dto';
+import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
-export type InsertVehicleRequestDto = z.infer<
-  typeof InsertVehicleRequestSchema
->;
+export class InsertVehicleRequestDto extends createZodDto(
+  InsertVehicleRequestSchema,
+) {}
+export class SearchVehiclesRequestDto extends createZodDto(
+  SearchVehiclesRequestSchema,
+) {}
+export class InsertVehicleExpenseRequestDto extends createZodDto(
+  InsertVehicleExpenseRequestSchema,
+) {}
+export class MakeVehicleSaleRequestDto extends createZodDto(
+  MakeVehicleSaleRequestSchema,
+) {}
+export class UpdateVehicleRequestDto extends createZodDto(
+  UpdateVehicleRequestSchema,
+) {}
+export class ToggleArchiveVehicleRequestDto extends createZodDto(
+  ToggleArchiveVehicleRequestSchema,
+) {}
+
 export type InsertVehicleResponseDto = z.infer<
   typeof InsertVehicleResponseSchema
->;
-
-export type SearchVehiclesRequestDto = z.infer<
-  typeof SearchVehiclesRequestSchema
 >;
 export type SearchVehiclesResponseDto = z.infer<
   typeof SearchVehiclesResponseSchema
 >;
-
-export type InsertVehicleExpenseRequestDto = z.infer<
-  typeof InsertVehicleExpenseRequestSchema
->;
 export type InsertVehicleExpenseResponseDto = z.infer<
   typeof InsertVehicleExpenseResponseSchema
->;
-
-export type FetchVehicleBrandsResponseDto = z.infer<
-  typeof FetchVehicleBrandsResponseSchema
->;
-
-export type MakeVehicleSaleRequestDto = z.infer<
-  typeof MakeVehicleSaleRequestSchema
 >;
 export type MakeVehicleSaleResponseDto = z.infer<
   typeof MakeVehicleSaleResponseSchema
 >;
-
-export type UpdateVehicleRequestDto = z.infer<
-  typeof UpdateVehicleRequestSchema
->;
 export type UpdateVehicleResponseDto = z.infer<
   typeof UpdateVehicleResponseSchema
 >;
-
-export type ToggleArchiveVehicleRequestDto = z.infer<
-  typeof ToggleArchiveVehicleRequestSchema
->;
 export type ToggleArchiveVehicleResponseDto = z.infer<
   typeof ToggleArchiveVehicleResponseSchema
+>;
+export type FetchVehicleBrandsResponseDto = z.infer<
+  typeof FetchVehicleBrandsResponseSchema
 >;
