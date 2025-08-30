da445ee90a4597fb1ef58ef6f62b4cc5eff133f6
MateusPitura <mateuspitura@gmail.com>
2025-08-30 16:14:32 -0300

    chore: biome lint

--- frontend/src/domains/vehicleSale/components/ViewVehicleSaleContainer.tsx
+++ frontend/src/domains/vehicleSale/components/ViewVehicleSaleContainer.tsx
@@ -1,14 +1,14 @@
+import Button from "@/design-system/Button";
 import Spinner from "@/design-system/Spinner";
 import PageHeader from "@/domains/global/components/PageHeader";
 import Section from "@/domains/global/components/Section";
-import { Vehicle } from "@/domains/global/types/model";
+import { VehicleWithPayment } from "@/domains/global/types/model";
 import { FuelType, VehicleCategory, VehicleStatus } from "@shared/enums";
 import { useQuery } from "@tanstack/react-query";
 import type { ReactNode } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import selectVehicleInfo from "../utils/selectVehicleInfo";
 import VehicleData from "./VehicleData";
-import Button from "@/design-system/Button";
 // import useSafeFetch from "@/domains/global/hooks/useSafeFetch";
 
 export default function ViewVehicleSaleContainer(): ReactNode {
@@ -17,45 +17,50 @@ export default function ViewVehicleSaleContainer(): ReactNode {
   // const { safeFetch } = useSafeFetch();
   const { vehicleSaleId } = useParams();
 
-  async function getVehicle(): Promise<Vehicle> {
-    // return await safeFetch(`${BACKEND_URL}/vehicle-sale/${vehicleSaleId}`, { // 🌠 MOCK
-    //   resource: "VEHICLE_SALE",
+  async function getVehicle(): Promise<VehicleWithPayment> {
+    // return await safeFetch(`${BACKEND_URL}/vehicle/${vehicleId}`, { // 🌠 MOCK
+    //   resource: "VEHICLES",
     //   action: "READ",
     // });
 
     return {
-      id: 1,
-      modelName: "Fusca",
-      announcedPrice: 9000000,
-      plateNumber: "ABC1234",
-      modelYear: 1970,
-      status: VehicleStatus.DELIVERED,
-      archivedAt: undefined,
-      brand: {
-        id: 10,
-        name: "Volkswagen",
+      payment: {
+        purchaseDate: "2023-01-01",
+        paidTo: "Fulano de Tal",
       },
-      store: {
+      vehicle: {
         id: 1,
-        name: "Loja 1",
+        modelName: "Fusca",
+        announcedPrice: 8000000,
+        plateNumber: "ABC1234",
+        modelYear: 1970,
+        status: VehicleStatus.DELIVERED,
+        archivedAt: undefined,
+        brand: {
+          id: 10,
+          name: "Volkswagen",
+        },
+        store: {
+          id: 1,
+          name: "Loja 1",
+        },
+        category: VehicleCategory.CAR,
+        color: "#FFFFFF",
+        chassiNumber: "AAAAAAAAAAAAAAAAA",
+        commissionValue: 1000,
+        fuelType: FuelType.FLEX,
+        kilometers: 1000,
+        minimumPrice: 8000000,
+        yearOfManufacture: 1970,
+        characteristics: [
+          "Direção hidráulica",
+          "Janelas elétricas",
+          "Ar condicionado",
+          "Piloto automático",
+          "Vidros elétricos",
+          "Freios ABS",
+        ],
       },
-      category: VehicleCategory.CAR,
-      color: "#FF0000",
-      chassiNumber: "AAAAAAAAAAAAAAAAA",
-      commissionValue: 0,
-      fuelType: FuelType.FLEX,
-      kilometers: 1000,
-      minimumPrice: 8000000,
-      yearOfManufacture: 1970,
-      characteristics: [
-        "Direção hidráulica",
-        "Janelas elétricas",
-        "Ar condicionado",
-        "Travas elétricas",
-        "Câmera de ré",
-        "Air bag",
-        "Rodas de liga leve",
-      ],
     };
   }
 
