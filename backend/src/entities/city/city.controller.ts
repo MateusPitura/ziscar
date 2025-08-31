9be32a889c9f0232f376b2e2d236ea671009b0f6
haasbriel <gabrieldasilva.haas@gmail.com>
2025-08-31 16:25:20 -0300

    adicionar o método de pagamento nas parcelas de uma conta a receber e adicionar o controller de city

--- backend/src/entities/city/city.controller.ts
+++ backend/src/entities/city/city.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CityService } from './city.service';
+import { BrazilianState } from '@prisma/client';
 
 @Controller('city')
-export class CityController {}
+export class CityController {
+    constructor(private readonly cityService: CityService) { }
+
+    @Get('/:state')
+    async getAllByState(@Param('state') state: BrazilianState) {
+        return this.cityService.getAllByState(state);
+    }
+}
