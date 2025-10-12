6290e67f4c82e99ca93d75094a73c4238313699b
MateusPitura <mateuspitura@gmail.com>
2025-10-12 14:48:37 -0300

    fix: added label to indicate that asterisk is required

--- frontend/src/domains/global/components/PageFooter.tsx
+++ frontend/src/domains/global/components/PageFooter.tsx
@@ -1,5 +1,7 @@
+import Icon from "@/design-system/Icon";
 import { ButtonState } from "@/design-system/types";
 import { Children, cloneElement, type ReactElement } from "react";
+import { BLANK } from "../constants";
 import useButtonState from "../hooks/useButtonState";
 import { Childrenable } from "../types";
 
@@ -22,6 +24,15 @@ export default function PageFooter({
 
   return (
     <div className="w-full p-4 pb-0 flex justify-end gap-4">
+      <span className="text-body-small text-red-500 flex items-center gap-1 flex-1">
+        <Icon
+          iconName="Emergency"
+          fontSize="inherit"
+          className="text-red-500 text-label-small"
+        />
+        {BLANK}
+        Campos obrigatórios
+      </span>
       {cloneElement(first as ReactElement, {
         state: primaryBtnStateParsed,
         type: "submit",
